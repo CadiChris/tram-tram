8d156790d2164813513bd8796a2a796705ed3ad7
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2021-10-23 23:19:52 +0200

    Heroku !

--- test-client/local.js
+++ test-client/local.js
@@ -1,5 +1,6 @@
 const WebSocket = require("ws");
 
-const socket = new WebSocket('ws://localhost:33290');
+// const socket = new WebSocket('ws://localhost:33290/web-socket');
+const socket = new WebSocket('ws://tram-tram.herokuapp.com/web-socket');
 
 socket.on('message', msg => console.log("%s", msg))
