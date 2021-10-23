5d173b31c1359465233db130a1e6512da7949823
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2021-10-23 22:00:09 +0200

    Adapter TBM du prochain passage

---
+++ server/prochainPassage.adapter.tbm.js
@@ -0,0 +1,21 @@
+const xml2js = require("xml2js");
+const prochainPassageAdapterTbm = {
+  async getProchainPassage({ http }) {
+    const xml = await http.get(
+      "https://data.bordeaux-metropole.fr/wps?key=258BILMNYZ&service=WPS&version=1.0.0&request=Execute&Identifier=saeiv_arret_passages&DataInputs=ARRET_ID=T_BQF_A"
+    );
+
+    const obj = await xml2js.parseStringPromise(xml);
+    const horaire_theorique =
+      obj["wps:ExecuteResponse"]["wps:ProcessOutputs"][0]["wps:Output"][0][
+        "wps:Data"
+      ][0]["wps:ComplexData"][0]["gml:featureMember"][0]["bm:HORAIRE"][0][
+        "bm:HOR_THEO"
+      ][0];
+    return { horaire_theorique };
+  },
+};
+
+module.exports = {
+  prochainPassageAdapterTbm,
+};
---
+++ server/tests/prochainPassage.adapter.tbm.test.js
@@ -0,0 +1,39 @@
+const { promises: fs } = require("fs");
+const path = require("path");
+const { prochainPassageAdapterTbm } = require("../prochainPassage.adapter.tbm");
+
+describe("Adapter du prochain passage sur TBM", () => {
+  it("appelle l'API TBM pour la Gare de Blanquefort", async () => {
+    const httpMock = {
+      get: jest.fn(async () => await xmlDuProchainPassage()),
+    };
+
+    await prochainPassageAdapterTbm.getProchainPassage({ http: httpMock });
+
+    expect(httpMock.get).toHaveBeenCalledWith(
+      "https://data.bordeaux-metropole.fr/wps?key=258BILMNYZ&service=WPS&version=1.0.0&request=Execute&Identifier=saeiv_arret_passages&DataInputs=ARRET_ID=T_BQF_A"
+    );
+  });
+
+  it("renvoie la date du prochain passage", async () => {
+    const httpMock = {
+      get: jest.fn(async () => await xmlDuProchainPassage()),
+    };
+
+    const prochain = await prochainPassageAdapterTbm.getProchainPassage({
+      http: httpMock,
+    });
+
+    expect(prochain).toEqual({
+      horaire_theorique: "2021-10-23T20:57:19",
+    });
+  });
+});
+
+async function xmlDuProchainPassage() {
+  const fichier = path.join(
+    __dirname,
+    "./data/tbm/prochain_passages_gare_de_blanquefort.xml"
+  );
+  return await fs.readFile(fichier);
+}
