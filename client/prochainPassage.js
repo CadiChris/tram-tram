1228d1f04de8cbb66d5ef003b9b7ac0cd79669af
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2021-10-26 00:26:54 +0200

    Afficher les différences d'horaire complètes, avec minutes et secondes
    On passe de qqch d'approximatif à une information complète et précise.

--- client/prochainPassage.js
+++ client/prochainPassage.js
@@ -15,16 +15,15 @@ function htmlPourUnHoraire(
 ) {
   const tram = new Date(horaire);
 
-  const diff_minutes = getDiffMinutes(reference, tram);
-  const diff_secondes = getDiffSecondes(reference, tram);
-  const moment_du_depart = Number(diff_minutes) - temps_de_marche;
+  const diff_avec_tram = getDiff(tram, reference);
+  const moment_du_depart = getDiff(tram - enMs(temps_de_marche), reference);
 
   return `
 <div class="un-depart">
   <div class="temps-de-marche">
-    ➡️ ${moment_du_depart} min. <sup>(+${temps_de_marche} min.)</sup>
+    ➡️ ${moment_du_depart} <sup>(+${temps_de_marche} min.)</sup>
   </div>
-  ⏳ ${diff_minutes} minutes (${diff_secondes} secondes) <br/>
+  ⏳ ${diff_avec_tram} <br/>
   ⌚️ <span class="secondaire">
     ${tram.toLocaleTimeString("fr")}
     <span class="terminus">${terminus}</span>
@@ -32,12 +31,22 @@ function htmlPourUnHoraire(
 </div>`;
 }
 
-function getDiffMinutes(reference, horaire) {
-  return Math.floor(Math.abs(reference - horaire) / 1000 / 60)
-    .toString()
-    .padStart(2, "0");
+export function getDiff(date_a, date_b) {
+  const diff_secondes = getDiffSecondes(date_a, date_b);
+  const minutes = Math.floor(diff_secondes / 60);
+  const secondes = diff_secondes % 60;
+  const signe = date_a < date_b ? "-" : "";
+  return `${signe}${deuxChiffres(minutes)} min. ${deuxChiffres(secondes)} s.`;
 }
 
 function getDiffSecondes(reference, horaire) {
   return Math.round(Math.abs(reference - horaire) / 1000);
+}
+
+function deuxChiffres(valeur) {
+  return valeur.toString().padStart(2, "0");
+}
+
+function enMs(temps_minutes) {
+  return temps_minutes * 60000;
 }
--- client/tests/prochainPassage.test.js
+++ client/tests/prochainPassage.test.js
@@ -1,4 +1,4 @@
-const { afficherProchainPassage } = require("../prochainPassage");
+const { afficherProchainPassage, getDiff } = require("../prochainPassage");
 
 describe("Affichage du prochain passage", () => {
   const arret_de_test = {
@@ -7,7 +7,7 @@ describe("Affichage du prochain passage", () => {
       { horaire: "2021-10-23T15:06:00", terminus: "Porte De B." },
     ],
     reference: new Date("2021-10-23T15:00:00"),
-    temps_de_marche: 1
+    temps_de_marche: 1,
   };
 
   it("affiche l'heure du prochain passage", () => {
@@ -26,8 +26,8 @@ describe("Affichage du prochain passage", () => {
 
     afficherProchainPassage(arret_de_test, updateUi);
 
-    assertAffichage("02 minutes (151 secondes)", updateUi);
-    assertAffichage("06 minutes (360 secondes)", updateUi);
+    assertAffichage("02 min. 31 s.", updateUi);
+    assertAffichage("06 min. 00 s.", updateUi);
   });
 
   it("affiche un conseil pour l'heure de mise en route", () => {
@@ -36,7 +36,18 @@ describe("Affichage du prochain passage", () => {
     afficherProchainPassage(arret_de_test, updateUi);
 
     assertAffichage("(+1 min.)", updateUi);
-    assertAffichage("1 min.", updateUi);
+    assertAffichage("01 min. 31 s.", updateUi);
+  });
+});
+
+describe("Différence entre 2 horaires", () => {
+  it("met un 'moins' devant la différence si l'horaire est dépassée", () => {
+    expect(
+      getDiff(new Date("2021-10-23T15:00:00"), new Date("2021-10-23T15:05:00"))
+    ).toBe("-05 min. 00 s.");
+    expect(
+      getDiff(new Date("2021-10-23T15:05:00"), new Date("2021-10-23T15:00:00"))
+    ).toBe("05 min. 00 s.");
   });
 });
 
