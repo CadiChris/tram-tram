b4caf449b81b1f231fc9344c45b1408f7473138e
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2021-10-26 10:00:29 +0200

    Retour à l'horaire applicable
    Les tests terrains montrent que l'horaire estimée est à l'ouest.

--- server/tbm/tests/prochainPassage.adapter.tbm.test.js
+++ server/tbm/tests/prochainPassage.adapter.tbm.test.js
@@ -29,34 +29,15 @@ describe("Adapter du prochain passage sur TBM", () => {
     );
 
     expect(prochain).toEqual([
-      { horaire: "2021-10-23T20:59:07", terminus: "Gare De Begles" },
-      { horaire: "2021-10-23T21:04:11", terminus: "Porte De Bourgogne" },
-      { horaire: "2021-10-23T21:17:50", terminus: "Gare De Begles" },
-    ]);
-  });
-
-  it("utilise l'horaire applicable quand l'horaire estimée est absente", async () => {
-    const httpMock = {
-      get: jest.fn(async () => await xmlSansHoraireEstimee()),
-    };
-
-    const prochain = await prochainPassageAdapterTbm.getProchainPassage(
-        { id_arret: "", terminus_exclus: ["Gare De Blanquefort"] },
-        { http: httpMock }
-    );
-
-    expect(prochain).toEqual([
       { horaire: "2021-10-23T21:05:25", terminus: "Gare De Begles" },
+      { horaire: "2021-10-23T21:11:43", terminus: "Porte De Bourgogne" },
+      { horaire: "2021-10-23T21:24:28", terminus: "Gare De Begles" },
     ]);
   });
 });
 
 async function xmlGareDeBlanquefort() {
   return await xmlDeTest("prochain_passages_gare_de_blanquefort.xml");
-}
-
-async function xmlSansHoraireEstimee() {
-  return await xmlDeTest("prochain_passages_sans_horaire_estimee.xml");
 }
 
 async function xmlDeTest(fichier) {
