c29cfd40539e6ac06c3c7ddadd0b6bdbe47ac7c3
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2021-10-27 00:29:33 +0200

    Remplace le compte-à-rebours pas une barre graphique

--- client/countdown.js
+++ client/countdown.js
@@ -1,15 +1,18 @@
-import { setHtml } from "./setHtml.js";
-
 export const countdown = {
   interval: null,
   start() {
     if (this.interval !== null) clearInterval(this.interval);
 
-    setHtml("#countdown")(5);
+    showProgress(5 * 20);
+
     let countdown = 4;
     this.interval = setInterval(() => {
-      setHtml("#countdown")(countdown);
+      showProgress(countdown * 20);
       countdown--;
     }, 1000);
   },
 };
+
+function showProgress(value) {
+  document.querySelector("progress").setAttribute("value", value);
+}
